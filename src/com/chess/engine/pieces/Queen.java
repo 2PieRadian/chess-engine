32b63d15ba8df93a738dc5800d1767af3fb5fb31
2PieRadian <kataware.doki2005@gmail.com>
2026-03-27 13:21:08 +0530

    Duplicate Sliding Logic Queen

--- src/com/chess/engine/pieces/Queen.java
+++ src/com/chess/engine/pieces/Queen.java
@@ -2,7 +2,9 @@ package com.chess.engine.pieces;
 
 import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
 import com.chess.engine.board.Move;
+import com.chess.engine.board.Tile;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -10,16 +12,48 @@ import java.util.List;
 
 public class Queen extends Piece {
 
-    private final static int[] CANDIDATE_MOVES = {};
+    private final static int[] CANDIDATE_MOVES = {-9, -8, -7, -1, 1, 7 ,8, 9};
 
     Queen(int coordinate, Alliance alliance) {
         super(coordinate, alliance);
+    }
+
+    private static boolean isFirstColumnExclusion(final int coordinate, final int offset) {
+        return BoardUtils.FIRST_COLUMN[coordinate] && (offset == -9 || offset == -1 || offset == 7);
+    }
+
+    private static boolean isEighthColumnExclusion(final int coordinate, final int offset) {
+        return BoardUtils.EIGHT_COLUMN[coordinate] && (offset == -7 || offset == 1 || offset == 9);
     }
 
     @Override
     public Collection<Move> calculateLegalMoves(Board board) {
         final List<Move> legalMoves = new ArrayList<>();
 
+        for (final int currentOffset : CANDIDATE_MOVES) {
+            int destinationCoordinate = this.getCoordinate();
+
+            while (true) {
+                if (isFirstColumnExclusion(destinationCoordinate, currentOffset)
+                        || isEighthColumnExclusion(destinationCoordinate, currentOffset))
+                    break;
+
+                destinationCoordinate += currentOffset;
+                if (!BoardUtils.isValidTileCoordinate(destinationCoordinate))
+                    break;
+
+                final Tile tileAtDestination = board.getTile(destinationCoordinate);
+                if (tileAtDestination.isTileEmpty()) {
+                    legalMoves.add(new Move.NormalMove(board, this, destinationCoordinate));
+                } else {
+                    Piece pieceAtDestination = tileAtDestination.getPiece();
+                    if (pieceAtDestination.getAlliance() != this.getAlliance())
+                        legalMoves.add(new Move.AttackMove(board, this, destinationCoordinate, pieceAtDestination));
+
+                    break;
+                }
+            }
+        }
 
         return List.copyOf(legalMoves);
     }
